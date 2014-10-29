4d2a1b356bfc604c72f76785c9a4e7f16bd8fff5
Antonino <admin@MacBook-di-Antonino.local>
2014-10-29 19:26:50 +0100

    Corretto println

--- Prova/ViewController.swift
+++ Prova/ViewController.swift
@@ -15,8 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         let rnd = arc4random_uniform(10)
-        println(rnd)
-        
+        println("num. casuale = \(rnd)")
         
     }
 
